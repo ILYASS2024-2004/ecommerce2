0980bd70eb4c2160d92798587d3c1a718103d97c
ILYASS2024-2004 <ilyass.elogri3@usmba.ac.ma>
2025-01-05 12:47:14 +0100

    first commit

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,20 +5,35 @@ import { Injectable } from '@angular/core';
 })
 export class ProductService {
   products = [
-    { id: 1, name: 'Chemise' , price :150 , image: 'https://www.nodus.fr/53609-product_zoom/chemise-chevron-bleu.jpg'},
+    {  name: 'Chemise' , price :150 , image: 'https://www.nodus.fr/53609-product_zoom/chemise-chevron-bleu.jpg'},
+    {  name: 'Chemise' , price :170 , image: 'https://th.bing.com/th/id/R.eace49725779b008983777dd94ec3461?rik=4o%2b0LBBn%2ffFtxA&pid=ImgRaw&r=0'},
+    {  name: 'Chemise' , price :120 , image: 'https://th.bing.com/th/id/OIP.RLG3g9chVAX8uixom7OlRQHaJj?rs=1&pid=ImgDetMain'},
+    {  name: 'Chemise' , price :130 , image: 'https://www.pyjama-orangerie.com/1323-thickbox_default/chemise-de-nuit-fille-bernadette-blanc.jpg'},
+    { id: 2, name: 'Pants' , price :220 , image: 'https://image.made-in-china.com/2f0j00KtUkDQEsqzqb/OEM-ODM-Non-Stretch-Quality-Straight-Fit-with-Patch-Pocket-on-Front-and-Side-High-Waisted-off-White-New-Fashion-Lady-Jeans.jpg'},
+
     { id: 2, name: 'Pants' , price :200 , image: 'https://th.bing.com/th/id/OIP.ZQhDuX5-lEbJGPCl3nH7iQAAAA?rs=1&pid=ImgDetMain'},
-    { id: 3, name: 'Pants jean' , price :240 , image: 'https://th.bing.com/th/id/OIP.QKkMIHylVrQoKN70ihEUtgHaLH?rs=1&pid=ImgDetMain'},
+    { id: 2, name: 'Pants' , price :300 , image: 'https://i.pinimg.com/originals/0a/75/d4/0a75d4079f753337b1134c340362f7ac.jpg'},
+
+    { id: 3, name: 'Pants' , price :240 , image: 'https://th.bing.com/th/id/OIP.QKkMIHylVrQoKN70ihEUtgHaLH?rs=1&pid=ImgDetMain'},
     { id: 4, name: 'T-shirt' , price :180 , image: 'https://th.bing.com/th/id/R.6c3ddd4f609acc5e3fff3e13255cfc15?rik=nMtuon3w6BIpiw&riu=http%3a%2f%2finikweb.com%2fwp-content%2fuploads%2f2015%2f02%2ft473kellygreen.jpg&ehk=ngEG5J4NY4mngHySwtGScfJOpm%2bFsWS9P0TPNj15Qww%3d&risl=&pid=ImgRaw&r=0'},
     { id: 5, name: 'T-shirt' , price :50 , image: 'https://artistfirst.com.au/cdn/shop/products/MassAppealDeadHeadsTeeFront_600x600.jpg?v=1655089182'},
+    { id: 5, name: 'T-shirt' , price :150 , image: 'https://th.bing.com/th/id/OIP.5XTM2YvfxcKZ889J0ENi-wHaLH?rs=1&pid=ImgDetMain'},
+
+    { id: 5, name: 'T-shirt' , price :50 , image: 'https://th.bing.com/th/id/OIP.YQV_az6sSDpnICHKr-dorAAAAA?rs=1&pid=ImgDetMain'},
+    
     { id: 6, name: 'Jacket' , price :550 , image: 'https://www.fashionbeans.com/wp-content/uploads/2022/12/axelarigato_pufferjacket.jpg'},
-    { id: 7, name: 'Jacket Jeans' , price :250 , image: 'https://th.bing.com/th/id/OIP.Bag_vP6lFp-jLYQVK_WLZwAAAA?rs=1&pid=ImgDetMain'},
-    { id: 8, name: 'Chosseur' , price :350 , image: 'https://th.bing.com/th/id/R.d43caea15b8643c913d021d98df9a1b3?rik=MZOHler1V24AgQ&pid=ImgRaw&r=0'},
-    { id: 9, name: 'Chosseur' , price :250 , image: 'https://th.bing.com/th/id/OIP.gQb-LXgLIxhF27l7XElnQgAAAA?rs=1&pid=ImgDetMain'},
-    { id: 10, name: 'Hodie' , price :190, image: 'https://d1l6fqa7jeeusv.cloudfront.net/upload/all/bRc6bCaq4wa9YTFz7qsqpkTuwJktb4N461d0omjS.jpg'},
+    { id: 6, name: 'Jacket' , price :350 , image: 'https://th.bing.com/th/id/OIP.EwxY1W9q8IkUYzC7D2k0ZwHaHY?rs=1&pid=ImgDetMain'},
+
+    { id: 6, name: 'Jacket' , price :400 , image: 'https://image1.superdry.com/static/images/optimised/zoom/upload9223368955666104448.jpg'},
+    { id: 7, name: 'Jacket' , price :250 , image: 'https://th.bing.com/th/id/OIP.Bag_vP6lFp-jLYQVK_WLZwAAAA?rs=1&pid=ImgDetMain'},
+   
+    { id: 10, name: 'Sweat-shirt' , price :190, image: 'https://d1l6fqa7jeeusv.cloudfront.net/upload/all/bRc6bCaq4wa9YTFz7qsqpkTuwJktb4N461d0omjS.jpg'},
     { id: 11, name: 'Sweat-shirt' , price :350 , image: 'https://th.bing.com/th/id/OIP.1vSZAfokwskYMfDQ_xF6KgHaHa?rs=1&pid=ImgDetMain'},
-    { id: 12, name: 'Nike' , price :350 , image: 'https://content.sitesupply.co/assets/89f6eaea-ce3b-4e93-9a85-28aaad2ce43f?width=1080&height=1080'},
-    { id: 13, name: 'Sandal' , price :60 , image: 'https://cf.shopee.co.id/file/319b3ed444033e9da5c790de1c3080eb'},
-    { id: 14, name: 'Foulard' , price :30 , image: 'https://th.bing.com/th/id/R.eb49e1873f2295f54da5273d9f0dc146?rik=ZrEH65PNU95aNg&pid=ImgRaw&r=0'},
+    { id: 13, name: 'Sweat-shirt' , price :160 , image: 'https://th.bing.com/th/id/R.2a479f8908637adbc80167f394a31e08?rik=bttPW2STSg2yuA&pid=ImgRaw&r=0'},
+    { id: 14, name: 'Sweat-shirt' , price :80 , image: 'https://www.alphabroder.com/media/hires/9103_51_z.jpg'},
+        { id: 12, name: 'Chausseur' , price :475 , image: 'https://content.sitesupply.co/assets/89f6eaea-ce3b-4e93-9a85-28aaad2ce43f?width=1080&height=1080'},
+{ id: 8, name: 'Chausseur' , price :350 , image: 'https://th.bing.com/th/id/R.d43caea15b8643c913d021d98df9a1b3?rik=MZOHler1V24AgQ&pid=ImgRaw&r=0'},
+    { id: 9, name: 'Chausseur' , price :250 , image: 'https://th.bing.com/th/id/OIP.gQb-LXgLIxhF27l7XElnQgAAAA?rs=1&pid=ImgDetMain'},
     { id: 15, name: 'Chausseur' , price :450 , image: 'https://www.commeuncamion.com/content/uploads/2014/02/chaussures-classe-brogues-septieme-largeur.jpg'},
   ];
   cart : any = [];
@@ -37,6 +52,7 @@ export class ProductService {
       this.cart.splice(i,1);
     }
   }
+ 
 
   constructor() { }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -10,12 +10,98 @@ export class ProductsComponent implements OnInit {
   products: any[] = [];
 
   constructor(private productService: ProductService) {}
+  chan(){
 
+    var up1:any = document.getElementById('up');
+    var crt:any = document.getElementById('crt');
+    if(scrollY==0 ){
+       up1.style.display='none';
+            crt.style.display='none';
+    }
+    window.onscroll= function() {
+        if(scrollY >= 500){
+            up1.style.display='block';
+            crt.style.display='block';
+            
+        }
+        else{
+            up1.style.display='none';
+            crt.style.display='none';
+            
+        }
+    }
+    // up1.onclick= function(){
+    
+    //     scroll(
+    //         {
+    //             top:0,
+    //             left:0,
+    //             behavior:"smooth"
+    //         }
+    
+    //     )
+    // }
+    up1.addEventListener('click',
+        function m(){
+              scroll(
+                {
+                     top:0,
+                     left:0,
+                     behavior:"smooth"
+                }
+               
+              )
+        }
+      
+    
+    )
+  }
+  ch(){
+    const text:any = "ILYASS_SHOP";
+      const  h1Element:any = document.getElementById("ilyass-shop");
+       var index:any = 0;
+  
+      function writeText() {
+        h1Element.textContent += text[index];
+        index++;
+  
+        // Recommence si tout est écrit
+        if (index === text.length) {
+          index = 0; // Réinitialise l'index pour un effet infini
+          setTimeout(() => {
+            h1Element.textContent = ""
+            h1Element.innerHTML = `<br>`
+            // Efface le texte avant de recommencer
+          }, 100); // Pause avant de recommencer
+        }
+      }
+  
+      setInterval(writeText, 500);
+  
+  }
+  
   ngOnInit() {
     this.products = this.productService.getProducts();
+    this.chan();
+    this.ch();
+     
   }
 
+pant=false;
+ chemis=false;
+ disp=true;
+ tsh=false;
+ sho=false;
+ swt=false;
+ jck=false;
+
+
+ 
+  
+ imagee= 'https://shop.rnli.org/cdn/shop/files/rnli-finisterre-fisherman-beanie-ecru-navy-rs2342112env-29583843426400_x1400.jpg?v=1696330665'
   addToCart(product: any) {
     this.productService.addTocart(product);
   }
+ 
 }
+
